f6218b529419b27101464f50281b0ea2b84d0d0f
BiancaZYCao <caozy@shanghaitech.edu.cn>
2020-08-13 15:41:02 +0800

    first try demo Web API

---
+++ Models/DbSeeder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class DbSeeder
+    {
+        public DbSeeder(TodoContext dbcontext)
+        {
+            /*
+            User user = new User()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "Daryl Kouk",
+                Username = "Daryl",
+                Password = encoder.Encode("Password")
+            };
+            dbcontext.Add(user);*/
+            TodoItem tdi = new TodoItem()
+            {
+                Name = "Cook dinner",
+                IsComplete = false,
+                Secret = "Dangerous Secret info"
+            };
+            dbcontext.Add(tdi);
+
+            dbcontext.SaveChanges();
+        }
+
+        
+    }
+}
